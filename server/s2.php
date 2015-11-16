d9dbe2ae8a56c824923365f567a17549f2f38590
Sunghun Kim <sunghunkim@Sunghuns-MacBook.local>
2015-11-16 22:03:45 +0900

    Added new s2

--- server/s2.php
+++ server/s2.php
@@ -14,6 +14,7 @@ if (!$colname) {
   $colname = 'housesale';
 }
 
+// New DB
 $collection = new MongoCollection($db, $colname . "_agg");
 
 //$query = ['amount'=>['$gt' => 0]] ;
@@ -22,7 +23,7 @@ $query = [] ;
 foreach ($_GET as $key => $value) {
   //echo "Key: $key; Value: $value<br />\n";
   if ($value == '') {
-    continue;
+    $query[$key] = null;
   }
 
   if ($key == 'debug') {
@@ -40,79 +41,11 @@ foreach ($_GET as $key => $value) {
   }
 }
 
-//$query = ['amount'=>['$gt' => 0]] ;
-//print_r($query);
+// find everything in the collection
+$cursor = $collection->find($query)->sort(['year'=>-11, 'month'=>-11]);
 
-/*
-db.housesale.aggregate(
-   [
-   { $match: { 'state': '서울특별시', year: { $gt: 2000 } } },
-      {
-        $group : {
-           _id : { "state": "$state", "city":"$city"},
-          
-           avgAmtArea: { $avg: { $divide: [ "$amount", "$area" ] } },
-           avgAmtLand: { $avg: { $divide: [ "$amount", "$landArea" ] } },
-          
-           avgAmt: { $avg: "$amount" },
-           count: { $sum: 1 }
-        }
-      }
-   ]
-)
-*/
+//echo json_encode(iterator_to_array($cursor),  
+echo json_encode(iterator_to_array($cursor),  
+  JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 
-//$query = ['state'=> '서울특별시'];
-
-$grouparr['_id'] = ['year' => '$year', 'month'=>'$month'];
-$grouparr['count'] = ['$sum' =>  '$count'];
-
-switch ($colname) {
-case 'aptsale':
-case 'flatsale':
-  $query['usedArea'] = ['$gt' => 0] ;
-  $grouparr['avgAmtUsed'] = ['$avg' => ['$divide' => [ '$amount', '$area' ] ] ];
-  break;
-
-case 'housesale':
-  $grouparr['avgAmtArea'] = ['$avg' =>  '$avgAmtArea' ] ;
-  $grouparr['avgAmtLand'] = ['$avg' =>  '$avgAmtLand' ] ;
-  break;
-
-case 'aptrent':
-case 'flatrent':
-  $query['usedArea'] = ['$gt' => 0] ;
-  $grouparr['avgAptRent'] = ['$avg' => ['$divide' => [ '$monthlyPay', '$area' ] ]];
-  $grouparr['avgAptDeposit'] = ['$avg' => ['$divide' => [ '$deposit', '$area' ] ] ];
-  break;
-
-case 'houserent':
-  $query['contractArea'] = ['$gt' => 0] ;
-  $grouparr['avgHouseDeposit'] = ['$avg' => ['$divide' => [ '$deposit', '$area' ] ] ];
-  $grouparr['avgHouseRent'] = ['$avg' => ['$divide' => [ '$monthlyPay', '$area' ] ] ];
-  break;
-}
-
-$ops = array();
-$ops[] = ['$match' => $query];
-$ops[] = ['$sort' => ['year'=> -1, 'month'=> -1]];
-$ops[] = ['$group' => $grouparr];
-
-$option = ['allowDiskUse' => true];
-
-if($debug) {
-  print_r($ops);
-}
-
-try {
- $cursor = $collection->aggregate($ops, $option);
-} catch (MongoException $e) {
-  echo "error message: ".$e->getMessage()."\n";
-  echo "error code: ".$e->getCode()."\n";
-  exit(1);
-}
-
-//echo json_encode(iterator_to_array($cursor), 	
-echo json_encode($cursor, 	
-	JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 ?>
