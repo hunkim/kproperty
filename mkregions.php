8ea8f3c176e9fc9df8731a36607851fe98a21701
Sung Kim <hunkim@gmail.com>
2015-11-17 16:45:17 +0800

    up

---
+++ mkregions.php
@@ -0,0 +1,112 @@
+<?php
+
+// connect
+$m = new MongoClient();
+// select a database
+$db = $m->selectDB('trend');
+
+function testgrp() {
+  // select a collection (analogous to a relational database's table)
+  $colnames = ['housesale', 'aptsale', 'flatsale', 'houserent', 'aptrent', 'flatrent'];
+
+  foreach ($colnames as $colname) {
+    $col2name = $colname."_reg";
+    $col2 = new MongoCollection($db, $col2name);
+
+    // Let's remove all first
+    $col2->remove([]);
+
+    // add agg information
+    mkgrp($db, $colname);
+  }
+}
+
+function mkreg($db, $colname) {
+  switch ($colname) {
+    case 'aptsale':
+    case 'flatsale':
+      $grouparr['avgAmtArea'] = ['$avg' => ['$divide' => [ '$amount', '$area' ] ] ];
+      $grouparr['_id'] = ['state'=>'$state',
+        'city'=>'$city', 'county'=>'$county', 'region'=>'$region', 'aptName'=>'$aptName',
+        'area'=>'$area'];
+
+      mkonereg($db, $colname, $grouparr);
+    break;
+
+    case 'housesale':
+
+      $grouparr['_id'] = ['state'=>'$state',
+        'city'=>'$city', 'county'=>'$county', 'region'=>'$region'];
+
+      mkonegrp($db, $colname, $grouparr);
+      break;
+
+case 'aptrent':
+case 'flatrent':
+      $grouparr['_id']= ['state'=>'$state',
+        'city'=>'$city', 'county'=>'$county', 'region'=>'$region', 'aptName'=>'$aptName',
+        'area'=>'$area'];
+
+      mkonegrp($db, $colname, $grouparr);
+      break;
+
+case 'houserent':
+      $grouparr['_id'] = ['state'=>'$state','city'=>'$city', 'county'=>'$county', 'region'=>'$region'];
+      mkonegrp($db, $colname, $grouparr);
+      break;
+  }
+}
+
+function makegrpIndex($db, $collection, $ids) {
+    $dbData = array();
+    foreach ($ids as $i => $field) {
+      $dbData[$i] = 1;
+    }
+
+    // Insert it to DB
+    //$r = $collection->createIndex($dbData, ['name'=> 'all']);
+    $r = $collection->createIndex($dbData);
+    echo $r;
+}
+
+function mkonegrp($db, $colname, $grouparr) {
+  $ops = array();
+//  $ops[] = ['$match' => $query];
+//  $ops[] = ['$sort' => ['year'=> -1, 'month'=> -1]];
+  $ops[] = ['$group' => $grouparr];
+
+  $option = ['allowDiskUse' => true];
+
+  //print_r($ops);
+
+  $collection = new MongoCollection($db, $colname);
+  echo("working on: $colname ... with");
+  print_r($grouparr['_id']);
+  makegrpIndex($db, $collection, $grouparr['_id']);
+
+  try {
+    $cursor = $collection->aggregate($ops, $option);
+  } catch (MongoException $e) {
+    echo "error message: ".$e->getMessage()."\n";
+    echo "error code: ".$e->getCode()."\n";
+    exit(1);
+  }
+
+  $col2name = $colname."_agg";
+  $col2 = new MongoCollection($db, $col2name);
+
+  $results = $cursor['result'];
+
+  foreach ($results as $key => $val) {
+    foreach($val as $skey=> $sval) {
+      if ($skey == '_id') {
+        $r = $sval;
+      } else {
+        $r[$skey] = $sval;
+      }
+    }
+    //print_r($r);
+    $col2->insert($r);
+  }
+}
+?>
