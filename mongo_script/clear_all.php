fdc382e9468367b3716c7f3aad014bdb6d691f70
Ubuntu <ubuntu@ip-172-31-8-96.us-west-2.compute.internal>
2015-11-17 06:40:40 +0000

    Added xls

---
+++ mongo_script/clear_all.php
@@ -0,0 +1,21 @@
+<?php
+
+// connect
+$m = new MongoClient();
+// select a database
+$db = $m->selectDB('trend');
+// select a collection (analogous to a relational database's table)
+$colnames = ['housesale', 'aptsale', 'flatsale', 'houserent', 'aptrent', 'flatrent'];
+
+foreach ($colnames as $colname) {
+  echo "Removing $colname...\n";
+  $col = new MongoCollection($db, $colname);
+  $col->remove([]);
+  $col->deleteIndexes();
+
+  $col2name = $colname."_agg";
+  $col2 = new MongoCollection($db, $col2name);
+  $col2->remove([]);
+  $col2->deleteIndexes();
+}
+?>
