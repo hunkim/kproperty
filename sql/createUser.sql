208faa9b7cadb5a1c0dd30ad07a1074a2b4e0958
Sung Kim <hunkim@gmail.com>
2015-11-20 15:05:32 +0800

    Added

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -3,4 +3,5 @@ CREATE USER 'trend@localhost' IDENTIFIED BY 'only!trend!';
 GRANT ALL PRIVILEGES ON trend.* TO 'trend'@'localhost';
 
 
-
+CREATE USER 'trend'@'%' IDENTIFIED BY 'only!trend!';
+GRANT ALL PRIVILEGES ON trend.* TO 'trend'@'%';
