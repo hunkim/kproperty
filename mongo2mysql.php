3eaf54a61b74d560b9578096fd1a5eac799f936a
Sung Kim <hunkim@gmail.com>
2015-11-20 14:43:14 +0800

    Added

--- mongo2mysql.php
+++ mongo2mysql.php
@@ -19,10 +19,45 @@ foreach ($colnames as $name) {
 function mongo2mysql($db, $colname, $year, $month) {
   $col = new MongoCollection($db, $colname);
   $cursor = $col->find()->timeout(-1)->limit(10);
-  foreach ($cursor as $idx => $doc) {
+  $idx = 0;
+  foreach ($cursor as $doc) {
+    if ($idx++===0) {
+        $table = createTable($colname, $doc);
+        echo ($table);
+    }
+
     foreach($doc as $key => $val) {
       echo ("[$idx] k: $key, v: $val (" . gettype($val) .")\n");
     }
+  }
+}
+
+fuction createTable($colname, $doc) {
+    $sql = "Create Table $colname (\n";
+    foreach($doc as $key => $val) {
+      $sqltype = getSQLType($val);
+      $sql .= "\t%key $sqltype,\n"
+    }
+    $sql .= ");";
+    return $sql;
+}
+
+function getSQLType ($val) {
+  $type = gettype($val);
+  switch($type)) {
+    case "integer":
+      return "int";
+    case "boolean":
+      return "int";
+    case "double":
+      reutrn "duble";
+    case "float":
+      retrun "float";
+    case "string":
+      return "varchar(255)";
+
+    default:
+    return "no support ($type)";
   }
 }
 
