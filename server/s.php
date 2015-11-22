e9c8b9d7c2b3bacadf78719541b71fe666319baa
Sung Kim <hunkim@gmail.com>
2015-11-22 16:06:04 +0800

    Added top 10

--- server/s.php
+++ server/s.php
@@ -20,7 +20,7 @@ switch($tname) {
 			$stat_sql .=	" from $tname where amount > 0 and year >= ? AND year <= ?";
 
 			$stat_simple = "select year, month, count, avgDeposit, avgRent from";
-			$stat_simple = "from $tname" . "_agg where year >= ? AND year <= ? order by year, month";
+			$stat_simple = "from $tname" . "_agg where k=? AND year >= ? AND year <= ?  order by year, month";
 		 break;
 
 	default:
@@ -30,7 +30,7 @@ switch($tname) {
 		$stat_sql .=	" from $tname where year >= ? AND year <= ?";
 
 		$stat_simple = "select year, month, count, avgDeposit, avgRent ";
-		$stat_simple = "from $tname" . "_agg where year >= ? AND year <= ? order by year, month";
+		$stat_simple = "from $tname" . "_agg where k=? AND year >= ? AND year <= ? order by year, month";
 }
 
 $stat_sql_append = " group by year, month order by year, month ";
@@ -82,7 +82,7 @@ function processQuery($sql, $sql_append, $simple) {
 
   if ($i<=3) {
 		$sql = $simple;
-		$params = [$searchKey, &$startyear, &$endyear];
+		$params = [&$searchKey, &$startyear, &$endyear];
 		$type ="sii";
 	}
 
