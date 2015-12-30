6eb399b391cb0c08ee02219cdeea4e62b86cb595
Sung Kim <hunkim@gmail.com>
2015-12-31 06:30:17 +0800

    server up

--- server/dbconn.php
+++ server/dbconn.php
@@ -4,7 +4,7 @@ function DBconn() {
   // Persistent Connections
   // http://stackoverflow.com/questions/3332074/what-are-the-disadvantages-of-using-persistent-connection-in-pdo
   // http://www.php.net/manual/en/mysqli.persistconns.php
-  $conn = new mysqli("p:localhost", "trend", "only!trend!", "ktrend");
+  $conn = new mysqli("p:localhost", "trend", "only!trend!", "rtrend");
 	// Check connection
 	if ($conn->connect_error) {
       if ($debug) {echo("Connection failed: " . $conn->connect_error);}
