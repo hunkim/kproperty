2bb1fe5bf91e78b1dc230e4fc74db77dbc2a8b26
Sung Kim <hunkim@gmail.com>
2015-12-31 21:17:14 +0800

    Added for server up

--- crawler/crawler.sh
+++ crawler/crawler.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-php crawler.php housesale&
-php crawler.php aptsale&
-php crawler.php flatsale&
-php crawler.php houserent&
-php crawler.php aptrent&
-php crawler.php flatrent&
-php crawler.php officetelrent&
-php crawler.php officetelsale&
-php crawler.php aptlots&
-php crawler.php landsale&
+php crawler.php housesale 2006&
+php crawler.php aptsale 2006&
+php crawler.php flatsale 2006&
+php crawler.php officetelsale 2006&
+php crawler.php aptlots 2006&
+php crawler.php landsale 2006&
+
+php crawler.php houserent 2010&
+php crawler.php aptrent 2010&
+php crawler.php flatrent 2010&
+php crawler.php officetelrent 2010&
