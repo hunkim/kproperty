01ce55016c21e6c34f285d836abd58d035ac7d68
Sung Kim <hunkim@gmail.com>
2015-12-31 18:01:18 +0800

    Added for server up

---
+++ crawler/crawler.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+php crawler.php housesale&
+php crawler.php aptsale&
+php crawler.php flatsale&
+php crawler.php houserent&
+php crawler.php aptrent&
+php crawler.php flatrent&
+php crawler.php officetelrent&
+php crawler.php officetelsale&
+php crawler.php aptlots&
+php crawler.php landsale&
