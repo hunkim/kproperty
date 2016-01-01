9d53704a281409074ce25e307d3080246b78be18
Sung Kim <hunkim@gmail.com>
2016-01-01 20:14:29 +0800

    Added for server up

--- server/top10.php
+++ server/top10.php
@@ -111,7 +111,7 @@ if ($result->num_rows > 0) {
 
 $result = [];
 foreach ($rows as $key => $val) {
-	$arr = ['c'=>[['v'=>$val['label']], ['v'=>$val['value'], ['v'=>'gold']]]];
+	$arr = ['c'=>[['v'=>$val['label']], ['v'=>$val['value']], ['v'=>'gold']]];
 	$result [] = $arr;
 }
 
