4afe3b2c113edabde4f89cd79212722624e91f5c
Sung Kim <hunkim@gmail.com>
2016-01-04 08:34:40 +0800

    Added monthly in top

---
+++ server/_log_.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once 'dbconn.php';
+
+
+$conn = DBConn();
+
+$sql = "select type, loc, count(*) as c from log group by type, loc";
+
+$result = $conn->query($sql);
+
+$rows=[];
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+    $rows[] = $row['v'];
+	}
+}
+
+// JSON_PRETTY_PRINT|
+print json_encode($rows,JSON_UNESCAPED_UNICODE);
+
+$conn->close();
+
+?>
