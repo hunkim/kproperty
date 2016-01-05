b88f1c2dfca3eda130696ae8c097bc15dc075576
Sung Kim <hunkim@gmail.com>
2016-01-05 16:49:35 +0800

    Added monthly in top

--- server/racephotoemail.php
+++ server/racephotoemail.php
@@ -1,17 +1,18 @@
 <?php
 
 # Include the Autoloader (see "Libraries" for install instructions)
+set_include_path('/home/ubuntu');
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
 # Instantiate the client.
-$mgClient = new Mailgun('YOUR_API_KEY');
-$domain = "YOUR_DOMAIN_NAME";
+$mgClient = new Mailgun('key-5feb0245349a1e6a92bf539c2a069733');
+$domain = "racephotos.org";
 
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
-    'from'    => 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
-    'to'      => 'Baz <YOU@YOUR_DOMAIN_NAME>',
+    'from'    => 'Race Photos <mailgun@racephoto.org>',
+    'to'      => '<hunkim@gmail.com>',
     'subject' => 'Hello',
     'text'    => 'Testing some Mailgun awesomness!'
 ));
